77bbd8808efad76d7b141be33cf84ae683dd4d8a
Dhruv Goradia <dhruvgoradia13@gmail.com>
2024-07-26 21:07:34 +0530

    updated links

--- src/data.js
+++ src/data.js
@@ -22,7 +22,7 @@ export const ProjectInfo = [
     {
         id: 1,
         prjDiscription: "This Weather App was built using <b>React JS</b> and <b>Open Weather API</b>. Building this project helped me to gain confidence on using APi and <b>Fetching and arranging the data</b> for a seamless look and better understandability of the data.Used <b>Tailwind CSS</b> to style it up and makt it reponsive to any width",
-        live: "weather-react-app-sigma-bice.vercel.app",
+        live: "https://weather-react-app-sigma-bice.vercel.app/",
         repoLink: "https://github.com/Dhruv13g45/Weather-react-app",
         path: "https://private-user-images.githubusercontent.com/126545959/328006382-ffbb8620-eb5c-4168-b982-f6db22ead82a.png?jwt=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJnaXRodWIuY29tIiwiYXVkIjoicmF3LmdpdGh1YnVzZXJjb250ZW50LmNvbSIsImtleSI6ImtleTUiLCJleHAiOjE3MjE3MzAwNTYsIm5iZiI6MTcyMTcyOTc1NiwicGF0aCI6Ii8xMjY1NDU5NTkvMzI4MDA2MzgyLWZmYmI4NjIwLWViNWMtNDE2OC1iOTgyLWY2ZGIyMmVhZDgyYS5wbmc_WC1BbXotQWxnb3JpdGhtPUFXUzQtSE1BQy1TSEEyNTYmWC1BbXotQ3JlZGVudGlhbD1BS0lBVkNPRFlMU0E1M1BRSzRaQSUyRjIwMjQwNzIzJTJGdXMtZWFzdC0xJTJGczMlMkZhd3M0X3JlcXVlc3QmWC1BbXotRGF0ZT0yMDI0MDcyM1QxMDE1NTZaJlgtQW16LUV4cGlyZXM9MzAwJlgtQW16LVNpZ25hdHVyZT05NzExOWExODEwMWZiMTI3OGVlZjdkNTY0OGMxYTQ5ZWQxMjY5Y2I0YTk5Y2EyNjlmMTEwNzcyYTQ3NmIyYTVmJlgtQW16LVNpZ25lZEhlYWRlcnM9aG9zdCZhY3Rvcl9pZD0wJmtleV9pZD0wJnJlcG9faWQ9MCJ9.kO2nDasXGcwlgoyY2VEZSg75rDysKsJfQuDRhjFj2bs",
         alt: "React Weather Project",
@@ -30,7 +30,7 @@ export const ProjectInfo = [
     {
         id: 2,
         prjDiscription: "The Todo React App, although a basic app but it was built to understand the flow of data from component to component and understand <b>Conditional Rendering</b>. This Projects was made using <b>Context API</b> and <b>Custom Hooks.(Internally using useState Hook)</b> with a additon of <b>LocalStorage</b> . I learnt how to create Custom Hooks and how can the code be more <b>Effective and Performance Optimized</b>.Used <b>Tailwind CSS</b> to style it up and makt it reponsive to any width",
-        live: "todo-app-repo-hazel.vercel.app",
+        live: "https://todo-app-repo-hazel.vercel.app/",
         repoLink: "https://github.com/Dhruv13g45/todo-app-repo",
         path: "https://private-user-images.githubusercontent.com/126545959/328006231-80e6a9ee-286b-4679-be5e-8aeba3ab9635.png?jwt=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJnaXRodWIuY29tIiwiYXVkIjoicmF3LmdpdGh1YnVzZXJjb250ZW50LmNvbSIsImtleSI6ImtleTUiLCJleHAiOjE3MjE3MzEwNTgsIm5iZiI6MTcyMTczMDc1OCwicGF0aCI6Ii8xMjY1NDU5NTkvMzI4MDA2MjMxLTgwZTZhOWVlLTI4NmItNDY3OS1iZTVlLThhZWJhM2FiOTYzNS5wbmc_WC1BbXotQWxnb3JpdGhtPUFXUzQtSE1BQy1TSEEyNTYmWC1BbXotQ3JlZGVudGlhbD1BS0lBVkNPRFlMU0E1M1BRSzRaQSUyRjIwMjQwNzIzJTJGdXMtZWFzdC0xJTJGczMlMkZhd3M0X3JlcXVlc3QmWC1BbXotRGF0ZT0yMDI0MDcyM1QxMDMyMzhaJlgtQW16LUV4cGlyZXM9MzAwJlgtQW16LVNpZ25hdHVyZT05NzY2YjNmMGQ0ZmMzZjRjMzAwY2NhMzdiOGE4MGIxMmFkMzM1YTMyZDVhYTAxMDUzMGIxMWMyZGY3ZDZlZWZjJlgtQW16LVNpZ25lZEhlYWRlcnM9aG9zdCZhY3Rvcl9pZD0wJmtleV9pZD0wJnJlcG9faWQ9MCJ9.PHdkpbWdXM41WGrU2CfrryIvdBiMOAK5dtAlmbLNZsA",
         alt: "React Todo App",
@@ -38,7 +38,7 @@ export const ProjectInfo = [
     {
         id: 3,
         prjDiscription: "This was basic E-Commerce Web page Design Challenge from FrontEnd Mentor that i completed initially. I used <b>Pure HTML CSS JavaScript</b> to build this projects. This project helped me to gain overall Proficiency in the core concepts as It also includes the <b>Carousal and Add To Cart Functionality.</b> This project gave me the assurity of having a good grip of fundamental topics.Used <b>Tailwind CSS</b> to style it up and makt it reponsive to any width",
-        live: "e-commerce-js-repo.vercel.app",
+        live: "https://e-commerce-js-repo.vercel.app/",
         repoLink: "https://github.com/Dhruv13g45/e-commerce-js-repo",
         path: "https://github.com/Dhruv13g45/e-commerce-js-repo/raw/main/design/desktop-preview.jpg",
         alt: "E-commerce Cart Page",
