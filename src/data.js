48eadbb101d7559a056d9f019d3f13588835e222
Dhruv Goradia <126545959+Dhruv13g45@users.noreply.github.com>
2024-11-13 21:27:12 +0530

    Update data.js

--- src/data.js
+++ src/data.js
@@ -21,6 +21,14 @@ export const contact = [
 export const ProjectInfo = [
     {
         id: 1,
+        prjDiscription: "Grab-Z-Lit is a project which offers a dynamic shopping experience built in React.js, ReduxToolkit, React Router, Tailwind CSS, React Toasts and Fake Store API.This project allows one for Product Browsing & Categorization, Wishlist & Cart Management, Product Review System, Order History and Real-Time Notifications. The key learnings from this project were State Management through Redux Toolkit, API integration Performance Optimization and Responsive Design.",
+        live: "https://grabzlit-ecommerce-store.vercel.app/",
+        repoLink: "https://github.com/Dhruv13g45/EcommerceStore",
+        path: "",
+        alt: "Grab-Z-Lit E-commerce Store",
+    }
+    {
+        id: 2,
         prjDiscription: "This Weather App was built using <b>React JS</b> and <b>Open Weather API</b>. Building this project helped me to gain confidence on using APi and <b>Fetching and arranging the data</b> for a seamless look and better understandability of the data.Used <b>Tailwind CSS</b> to style it up and makt it reponsive to any width",
         live: "https://weather-react-app-sigma-bice.vercel.app/",
         repoLink: "https://github.com/Dhruv13g45/Weather-react-app",
@@ -28,7 +36,7 @@ export const ProjectInfo = [
         alt: "React Weather Project",
     },
     {
-        id: 2,
+        id: 3,
         prjDiscription: "The Todo React App, although a basic app but it was built to understand the flow of data from component to component and understand <b>Conditional Rendering</b>. This Projects was made using <b>Context API</b> and <b>Custom Hooks.(Internally using useState Hook)</b> with a additon of <b>LocalStorage</b> . I learnt how to create Custom Hooks and how can the code be more <b>Effective and Performance Optimized</b>.Used <b>Tailwind CSS</b> to style it up and makt it reponsive to any width",
         live: "https://todo-app-repo-hazel.vercel.app/",
         repoLink: "https://github.com/Dhruv13g45/todo-app-repo",
@@ -36,7 +44,7 @@ export const ProjectInfo = [
         alt: "React Todo App",
     },
     {
-        id: 3,
+        id: 4,
         prjDiscription: "This was basic E-Commerce Web page Design Challenge from FrontEnd Mentor that i completed initially. I used <b>Pure HTML CSS JavaScript</b> to build this projects. This project helped me to gain overall Proficiency in the core concepts as It also includes the <b>Carousal and Add To Cart Functionality.</b> This project gave me the assurity of having a good grip of fundamental topics.Used <b>Tailwind CSS</b> to style it up and makt it reponsive to any width",
         live: "https://e-commerce-js-repo.vercel.app/",
         repoLink: "https://github.com/Dhruv13g45/e-commerce-js-repo",
